3073f817f5320cc5137a084090da5b0a23698c1b
tuanhnt1712 <tuanhnt1712@gmail.com>
2017-06-28 16:31:30 +0700

    init model

---
+++ db/migrate/20170627072139_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :picture
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :posts, [:user_id, :created_at]
+  end
+end
